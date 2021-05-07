37e60979d42eabc146d4245f35e58e8d8de81285
Rouven <rouven.daunke@googlemail.com>
2021-05-07 15:01:38 +0200

    Changes to styling, default content setup local mongoDB

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "This is a simple blog website I built while learning backend development with node.js. The blog uses a mongodb database that I set-up with mongoose.";
+const aboutContent = "Hi, I'm Rouven. Currently learning full stack web development via an online bootcamp. During day time I'm working as a project manager in the healthcare sector, in the evenings I learn how to code or work on my side project: Hiroi: Hiring Open Innovation.";
+const contactContent = "You can reach out to me via the channels below.";
 
-let posts = [];
 
 const app = express();
 
@@ -18,52 +18,68 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-  res.render("home", {
-                      homeStartingContentEjs: homeStartingContent,
-                      posts: posts,
-                    });
 
+// mongoose.connect("mongodb://localhost:27017/blogPostsDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+const blogPostSchema = new mongoose.Schema({
+  title: String,
+  content: String
 });
+
+
+const BlogPost = mongoose.model("BlogPost", blogPostSchema);
+
+
+app.get("/", function(req, res){
+  BlogPost.find({}, function(err, foundBlogPost){
+    res.render("home", {
+                        homeStartingContentEjs: homeStartingContent,
+                        posts: foundBlogPost,
+    });
+  });
+});
+
 
 app.get("/about", function(req, res){
   res.render("about", {aboutContentEjs: aboutContent});
 });
 
+
 app.get("/contact", function(req, res){
   res.render("contact", {contactContentEjs: contactContent});
 });
+
 
 app.get("/compose", function(req, res){
   res.render("compose");
 });
 
+
 app.post("/compose", function(req, res){
-  const post = {
+  BlogPost.create([{
     title: req.body.inputBlogTitle,
-    content: req.body.inputBlogPost
-  };
-  posts.push(post);
-
-  res.redirect("/");
-});
-
-app.get("/posts/:postName", function(req, res){
-  let requestedTitle = _.lowerCase(req.params.postName);
-  posts.forEach(function(post) {
-    let postLink = _.lowerCase(post.title)
-    if (requestedTitle === postLink) {
-      res.render("post", {
-        title: post.title,
-        content: post.content});
-    };
+    content: req.body.inputBlogPost,
+  }], function(err, savedPost){
+    if(!err){
+      res.redirect("/");
+    }
   });
-
 });
 
 
-
-
+app.get("/posts/:postID", function(req, res){
+  const requestedPostId = req.params.postID;
+  BlogPost.findOne({_id: requestedPostId}, function(err, foundBlogPost){
+    if(!err){
+      res.render("post", {
+        title: foundBlogPost.title,
+        content: foundBlogPost.content
+      });
+    }
+  });
+});
 
 
 app.listen(3000, function() {
